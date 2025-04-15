07432c875033d0062069c17cd2ee5d7a426d639a
SantiagoMontanari <santimonta99@hotmail.com>
2025-04-15 18:46:15 -0300

    [parte 3.3]modifico urls y agrego details.html

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from .models import Question
 from django.shortcuts import render
+from django.http import Http404
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
@@ -8,7 +9,11 @@ def index(request):
     return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
